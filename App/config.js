494ec5dc97423ce872b83f79bb357650fadafe12
samlinnemann <157145332+samlinnemann@users.noreply.github.com>
2025-03-19 10:40:22 -0400

    sam update

--- App/config.js
+++ App/config.js
@@ -1,2 +1,2 @@
 // config.js
-export const API_URL = 'https://1c55-2601-5cf-4200-88f0-a55b-6ddd-fc24-542d.ngrok-free.app/api';
+export const API_URL = 'https://d584-108-44-195-179.ngrok-free.app/api';
